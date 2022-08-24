873dc1a8183442bff0998155f6a829e5ae259e53
Abu <abughalib64@gmail.com>
2022-08-24 17:48:24 +0530

    code cleanup

--- index.ts
+++ index.ts
@@ -1,17 +1,29 @@
-import DiscordJS, { Client, IntentsBitField } from 'discord.js';
-
+import { Client, GatewayIntentBits, IntentsBitField } from "discord.js";
+import setCommands from "./commands/commands";
+import handleInteractions from "./commands/interactions";
+import handleMessage from "./commands/messages";
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMessages
-    ]
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-client.on('ready', ()=> {
-    console.log("The bot is ready!");
-    const guildId = '923701565158744155';
-    const guild = client.guilds.cache.get(guildId);
+client.on("ready", () => {
+  console.log("The bot is ready!");
+  const guildId = "923701565158744155";
+  setCommands(client, guildId);
 });
 
-client.login(process.env.LOOKING_BOT_TEAM_TOKEN)
+client.on("interactionCreate", async (interaction) => {
+  await handleInteractions(interaction);
+});
+
+client.on("messageCreate", async (message) => {
+  let prefix = "!";
+  await handleMessage(prefix, message, client);
+});
+
+client.login(process.env.LOOKING_BOT_TEAM_TOKEN);
