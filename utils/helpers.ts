84b7361c6e80008b84baffb53805222ce7139e78
Abu <abughalib64@gmail.com>
2022-08-25 17:46:52 +0530

    round of decimal

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,5 +1,4 @@
 function formatTime(duration: number): string {
-
   // Added for cameld00d
   if (duration < 0) {
     return "For few hours maybe";
@@ -11,15 +10,15 @@ function formatTime(duration: number): string {
     if (duration < 1) {
       return `${duration * 60} seconds`;
     }
-    return `About ${duration} Minutes`;
+    return `About ${duration.toFixed(2)} Minutes`;
   }
   let str_duration = duration.toString();
 
   if (str_duration.includes(".")) {
     const duration_split = str_duration.split(".");
     const hours = duration_split[0];
-    const minutes = (parseInt(duration_split[1]) * 60)/10;
-    return `About ${hours} hours and ${minutes} Minutes`;
+    const minutes = (parseInt(duration_split[1]) * 60) / 10;
+    return `About ${hours} hours and ${minutes.toFixed(2)} Minutes`;
   }
 
   return `About ${duration} Hours`;
