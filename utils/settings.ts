11efc5df9e37e78c244e013910f040160462fb56
Abu <abughalib64@gmail.com>
2022-08-25 10:08:18 +0530

    added more constant

--- utils/settings.ts
+++ utils/settings.ts
@@ -1,7 +1,12 @@
 export class AppSettings {
   static readonly EMBEDED_MESSAGE_COLOR = 0x0099ff;
-  static readonly HOURS_TO_MILISEC = 60 * 60 * 1000;
-  static readonly HELP_MESSAGE_DISMISS_DURATION = 60*1000;
+  static readonly HOURS_TO_MILISEC = 60 * 60 * 1000; // 3600 seconds
+  static readonly HELP_MESSAGE_DISMISS_DURATION = 60*1000; // 60 seconds
+  static readonly DEFAULT_REQUEST_TEAM_TIMEOUT = 10*60*1000; // 10 Minutes
   static readonly MAXIMUM_TEAM_SPOT = 3;
   static readonly MAXIMUM_HOURS_TEAM = 10;
+  static readonly DEFAULT_GAME_VERSION = "Odyssey";
+  static readonly DEFAULT_TEAM_ACTIVITY = "Any";
+  static readonly DEFAULT_TEAM_LOCATION = "Anywhere";
+  static readonly DEFAULT_TEAM_DURATION = 1;
 }
