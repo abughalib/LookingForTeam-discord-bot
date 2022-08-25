6f349186a7f25e451590bab7e3458e64a7b4f12f
Abu <abughalib64@gmail.com>
2022-08-25 17:47:01 +0530

    name change

--- utils/settings.ts
+++ utils/settings.ts
@@ -1,8 +1,8 @@
 export class AppSettings {
   static readonly EMBEDED_MESSAGE_COLOR = 0x0099ff;
   static readonly HOURS_TO_MILISEC = 60 * 60 * 1000; // 3600 seconds
-  static readonly HELP_MESSAGE_DISMISS_DURATION = 60*1000; // 60 seconds
-  static readonly DEFAULT_REQUEST_TEAM_TIMEOUT = 10*60*1000; // 10 Minutes
+  static readonly HELP_MESSAGE_DISMISS_TIMEOUT = 60 * 1000; // 60 seconds
+  static readonly DEFAULT_REQUEST_TEAM_TIMEOUT = 10 * 60 * 1000; // 10 Minutes
   static readonly MAXIMUM_TEAM_SPOT = 3;
   static readonly MAXIMUM_HOURS_TEAM = 10;
   static readonly DEFAULT_GAME_VERSION = "Odyssey";
