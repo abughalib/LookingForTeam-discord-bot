f949d05564b0d44b4887743fc57c2e6654dbd0f7
Abu <abughalib64@gmail.com>
2022-08-25 17:43:15 +0530

    removed message interactions

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
 import { Client, GatewayIntentBits, IntentsBitField } from "discord.js";
 import setCommands from "./handlers/commands";
 import handleInteractions from "./handlers/interactions";
-import handleMessage from "./handlers/messages";
 
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMessages,
-    GatewayIntentBits.MessageContent,
   ],
 });
 
@@ -20,9 +18,4 @@ client.on("interactionCreate", async (interaction) => {
   await handleInteractions(interaction);
 });
 
-client.on("messageCreate", async (message) => {
-  let prefix = "!";
-  await handleMessage(prefix, message, client);
-});
-
-client.login(process.env.LOOKING_BOT_TEAM_TOKEN);
+client.login(process.env.TEST_BOT_TOKEN);
