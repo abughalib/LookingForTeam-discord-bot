207aa4cc0797be1faa011d361e9d09dfbe51803c
Abu <abughalib64@gmail.com>
2022-08-26 10:56:47 +0530

    more const

--- utils/settings.ts
+++ utils/settings.ts
@@ -9,4 +9,11 @@ export class AppSettings {
   static readonly DEFAULT_TEAM_ACTIVITY = "Any";
   static readonly DEFAULT_TEAM_LOCATION = "Anywhere";
   static readonly DEFAULT_TEAM_DURATION = 1;
+  static readonly PC_WING_REQUEST_INTERACTION_TITLE = "PC Team + Wing Request";
+  static readonly PC_CHANNEL_ID = "1012258271509356604";
+  static readonly XBOX_WING_REQUEST_INTERACTION_TITLE = "XBOX Wing Request";
+  static readonly XBOX_CHANNEL_ID = "790162689887961089";
+  static readonly PS_WING_REQUEST_INTERACTION_TITLE =
+    "Play Station Wing Request";
+  static readonly PS_CHANNEL_ID = "790162405342707713";
 }
