25f03e0b54c166e552f5bc0c7c3046d059787c4d
Abu <abughalib64@gmail.com>
2022-09-06 19:01:09 +0530

    added accept/decline

--- handlers/interactions.ts
+++ handlers/interactions.ts
@@ -10,6 +10,8 @@ import {
   EmbedBuilder,
   Embed,
   Message,
+  APIEmbedField,
+  CacheType,
 } from "discord.js";
 import { AppSettings } from "../utils/settings";
 import formatTime from "../utils/helpers";
@@ -93,7 +95,7 @@ async function interactionCommandHandler(
       return;
     }
 
-    const options_values = [activity, location, spots];
+    const options_values = [activity, location, parseInt(spots.toString())];
 
     let title: string = AppSettings.PC_WING_REQUEST_INTERACTION_TITLE;
 
@@ -232,34 +234,54 @@ async function interactionButtonHandler(interaction: ButtonInteraction) {
       return;
     }
 
+    if (interaction.message.embeds[0].data.fields === undefined) {
+      return;
+    }
+
     let originalUserInteraction = interaction.message.interaction.user;
     let currentUserInteraction = interaction.user;
 
-    interaction.reply({
-      content: `Hey ${originalUserInteraction}, ${currentUserInteraction} is looking for Team Invite`,
+    let originalFields: APIEmbedField[] =
+      interaction.message.embeds[0].data.fields;
+
+    let spots: number = 0;
+
+    originalFields.forEach((field) => {
+      if (field.name === "Number of Space in Wing/Team Available") {
+        spots = parseInt(field.value);
+      }
     });
 
-    let message = await interaction.fetchReply();
-
-    setTimeout(async () => {
-      await interaction.deleteReply().catch((error) => {
-        // Message Already Deleted
-        if (error.code === 10008) {
-          console.info("Message Already Deleted");
-        } else if (error.code === 50027) {
-          // Discord API Error, Invalid Webhook Token
-          deleteMessage(message);
-        } else {
-          // Message Deletion failed and its unknown Error.
-          console.error(`Failed to to delete the message: ${error}`);
-        }
+    if (spots <= 0) {
+      interaction.reply({
+        ephemeral: true,
+        content: "Sorry, but the team is already full.",
       });
-    }, AppSettings.DEFAULT_REQUEST_TEAM_TIMEOUT);
-  }
+      return;
+    }
 
-  if (interaction.customId === "command_dismiss") {
+    let buttons = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      new ButtonBuilder()
+        .setCustomId("accept_request")
+        .setLabel("Accept Request")
+        .setStyle(ButtonStyle.Primary),
+      new ButtonBuilder()
+        .setCustomId("reject_request")
+        .setLabel("Decline/Cancel Request")
+        .setStyle(ButtonStyle.Danger)
+    );
+
+    interaction.reply({
+      content: `Hey ${originalUserInteraction}, ${currentUserInteraction} is looking for Team Invite`,
+      components: [buttons],
+    });
+    deleteInteractionButton(
+      interaction,
+      AppSettings.DEFAULT_REQUEST_TEAM_TIMEOUT
+    );
+  } else if (interaction.customId === "command_dismiss") {
     // If Interaction message is null or delete by admin.
-    if (interaction.message.interaction == null) {
+    if (interaction.message.interaction === null) {
       interaction.reply({
         content: "Cannot perform this action",
         ephemeral: true,
@@ -282,12 +304,128 @@ async function interactionButtonHandler(interaction: ButtonInteraction) {
         ephemeral: true,
       });
       return;
+    } else {
+      interaction.reply({
+        content: "Cannot perform this action",
+        ephemeral: true,
+      });
+    }
+  } else if (
+    interaction.customId === "accept_request" ||
+    interaction.customId === "reject_request"
+  ) {
+    if (interaction.message.reference === null) {
+      console.error("Interaction is null: " + interaction.message);
+      return;
+    }
+    if (interaction.message.reference.messageId === undefined) {
+      console.error(
+        "Interaction Reference MessageId undefined: " +
+          interaction.message.reference
+      );
+      return;
+    }
+    if (interaction.channel === null) {
+      console.error("Interaction Channel is null: " + interaction);
+      return;
     }
 
-    interaction.reply({
-      content: "Cannot perform this action",
-      ephemeral: true,
-    });
+    let messageId: string = interaction.message.reference.messageId;
+    let message: Message | null = await interaction.channel.messages
+      .fetch(messageId)
+      .catch((error) => {
+        console.log("Cannot find message: " + error);
+        return null;
+      });
+    if (message === null || message.interaction == null) {
+      return;
+    }
+
+    if (interaction.customId === "accept_request") {
+      if (interaction.user === message.interaction.user) {
+        // Edit the interection
+        // and delete the reply
+
+        let original_message: Embed = message.embeds[0];
+        let title = original_message.data.title;
+        let author = original_message.data.author?.name || "";
+        let fields = original_message.data.fields;
+        let timestamp = original_message.timestamp;
+
+        if (!title || !author || !fields || !timestamp) {
+          console.error("Cannot find original interaction embed fields");
+          return;
+        }
+
+        if (fields === undefined) {
+          return;
+        }
+
+        let spots_count: number = 0;
+        let new_fields: APIEmbedField[] = [];
+
+        fields.forEach((field) => {
+          if (field.name === "Number of Space in Wing/Team Available") {
+            new_fields.push({
+              name: "Number of Space in Wing/Team Available",
+              value: (parseInt(field.value) - 1).toString(),
+            });
+          } else {
+            new_fields.push(field);
+          }
+        });
+
+        let footer = original_message.data.footer?.text || "";
+
+        let new_embeded_message = new EmbedBuilder();
+        new_embeded_message.setTitle(title || "");
+        new_embeded_message.setAuthor({ name: author });
+        new_embeded_message.addFields(new_fields || [{ name: "", value: "" }]);
+        new_embeded_message.setFooter({ text: footer });
+        new_embeded_message.setTimestamp(Date.parse(timestamp));
+
+        if (fields === undefined) {
+          return;
+        }
+
+        await message.edit({
+          embeds: [new_embeded_message],
+        });
+
+        await interaction.reply({
+          ephemeral: false,
+          content: `${interaction.message.author}, Your request is accepted`,
+        });
+        deleteInteractionButton(
+          interaction,
+          AppSettings.HELP_MESSAGE_DISMISS_TIMEOUT
+        );
+        deleteMessage(interaction.message);
+      } else {
+        await interaction.reply({
+          ephemeral: true,
+          content: `You Cannot Perform this action`,
+        });
+      }
+    } else if (interaction.customId === "reject_request") {
+      // Reject request
+      // It should be done by either original author or the one requested.
+      if (interaction.message.mentions.users.has(interaction.user.id)) {
+        interaction.reply({
+          ephemeral: true,
+          content: "Request Cancelled",
+        });
+        deleteMessage(interaction.message);
+      } else {
+        interaction.reply({
+          ephemeral: true,
+          content: "You cannot perform this action",
+        });
+      }
+    }
+  } else {
+    // If More features are required
+    // Would be Implemented later...
   }
 }
 
@@ -311,63 +449,57 @@ async function interactionMenuHandler(
   let author = original_message.data.author?.name || "";
   let fields = original_message.data.fields;
   let footer = original_message.data.footer?.text || "";
+  let timestamp = original_message.data.timestamp;
+
+  if (!title || !author || !fields || !footer || !timestamp) {
+    console.error("Cannot find original interaction message");
+    return;
+  }
 
   let new_embeded_message = new EmbedBuilder();
-  new_embeded_message.setTitle(title || "");
+  new_embeded_message.setTitle(title);
   new_embeded_message.setAuthor({ name: author });
-  new_embeded_message.addFields(fields || [{ name: "", value: "" }]);
+  new_embeded_message.addFields(fields);
   new_embeded_message.setFooter({ text: footer });
-  new_embeded_message.setTimestamp();
+  new_embeded_message.setTimestamp(Date.parse(timestamp) || Date.now());
 
   if (interaction.customId === "select_game_version") {
     await interaction.deferUpdate();
-    if (interaction.values[0] === "odyssey") {
-      new_embeded_message.addFields({
-        name: "Elite Dangerous Version",
-        value: "Elite Dangerous Odyssey",
-      });
-      await interaction.editReply({
-        components: [buttons],
-        embeds: [new_embeded_message],
-      });
-    } else if (interaction.values[0] === "horizon_four_zero") {
-      new_embeded_message.addFields({
-        name: "Elite Dangerous Version",
-        value: "Elite Dangerous Horizon 4.0",
-      });
-      await interaction.editReply({
-        components: [buttons],
-        embeds: [new_embeded_message],
-      });
-    } else if (interaction.values[0] === "horizon_three_eight") {
-      new_embeded_message.addFields({
-        name: "Elite Dangerous Version",
-        value: "Elite Dangerous Horizon 3.8",
-      });
-      await interaction.editReply({
-        components: [buttons],
-        embeds: [new_embeded_message],
-      });
-    } else if (interaction.values[0] === "beyond") {
-      new_embeded_message.addFields({
-        name: "Elite Dangerous Version",
-        value: "Elite Dangerous Beyond",
-      });
-      await interaction.editReply({
-        components: [buttons],
-        embeds: [new_embeded_message],
-      });
-    } else if (interaction.values[0] === "any") {
-      new_embeded_message.addFields({
-        name: "Elite Dangerous Version",
-        value: "Any",
-      });
-      await interaction.editReply({
-        components: [buttons],
-        embeds: [new_embeded_message],
-      });
-    }
+
+    AppSettings.AVAILABLE_GAME_VERSIONS.forEach(async (version) => {
+      if (version.value === interaction.values[0]) {
+        new_embeded_message.addFields({
+          name: AppSettings.GAME_NAME + " Version",
+          value: version.description,
+        });
+        await interaction.editReply({
+          components: [buttons],
+          embeds: [new_embeded_message],
+        });
+      }
+    });
   }
+}
+
+async function deleteInteractionButton(
+  interaction: ButtonInteraction<CacheType>,
+  timeout: number
+) {
+  let message = await interaction.fetchReply();
+  setTimeout(async () => {
+    await interaction.deleteReply().catch((error) => {
+      // Message Already Deleted
+      if (error.code === 10008) {
+        console.info("Message Already Deleted");
+      } else if (error.code === 50027) {
+        // Discord API Error, Invalid Webhook Token
+        deleteMessage(message);
+      } else {
+        // Message Deletion failed and its unknown Error.
+        console.error(`Failed to to delete the message: ${error}`);
+      }
+    });
+  }, timeout);
 }
 
 async function deleteMessage(message: Message | null | undefined) {
