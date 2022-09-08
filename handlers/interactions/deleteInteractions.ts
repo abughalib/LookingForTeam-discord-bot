7c7f32a5e2cb642fa4c6e721ff2325b484cf64f5
Abu <abughalib64@gmail.com>
2022-09-08 18:43:46 +0530

    cleanup

--- handlers/interactions/deleteInteractions.ts
+++ handlers/interactions/deleteInteractions.ts
@@ -1,8 +1,8 @@
-import { ButtonInteraction, CacheType } from "discord.js";
+import { ButtonInteraction, CacheType, CommandInteraction } from "discord.js";
 import deleteMessage from "./deleteMessage";
 
-async function deleteInteractionButton(
-  interaction: ButtonInteraction<CacheType>,
+async function deleteInteraction(
+  interaction: ButtonInteraction<CacheType> | CommandInteraction<CacheType>,
   timeout: number
 ) {
   let message = await interaction.fetchReply();
@@ -22,4 +22,26 @@ async function deleteInteractionButton(
   }, timeout);
 }
 
-export default deleteInteractionButton;
+async function deleteInteractionCommand(
+  interaction: CommandInteraction<CacheType>,
+  timeout: number
+) {
+  let message = await interaction.fetchReply();
+  setTimeout(async () => {
+    await interaction.deleteReply().catch((error) => {
+      // Message Already Deleted
+      if (error.code === 10008) {
+        console.info("Message Already Deleted");
+      } else if (error.code === 50027) {
+        // Discord API Error, Invalid Webhook Token
+        deleteMessage(message);
+      } else {
+        // Message Deletion failed and its unknown Error.
+        console.error(`Failed to to delete the message: ${error}`);
+      }
+    });
+  }, timeout);
+}
+
+// export {deleteInteractionCommand, deleteInteractionButton};
+export default deleteInteraction;
