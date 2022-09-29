d949832e675ce724f7e2a714a6680e47501e62ac
Abu <abughalib64@gmail.com>
2022-09-30 04:37:58 +0530

    added add, update, delete channel

--- utils/database.ts
+++ utils/database.ts
@@ -1,14 +1,27 @@
 import sqlite3 from "sqlite3";
-import { open } from "sqlite";
+import { ISqlite, open } from "sqlite";
 
-enum ChannelType {
-  PC,
-  XBOX,
-  PS,
-  OTHER,
+/*
+CREATE TABLE channelIds IF NOT EXISTS(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    channelId VARCHAR(255) NOT NULL,
+    channelType VARCHAR(50) NOT NULL,
+    channelName varchar(50) NOT NULL,
+    UNIQUE(channelId)
+);
+
+Channel Info from database schema
+*/
+
+interface ChannelInfo {
+  id: number;
+  channelId: string;
+  channelType: string;
+  channelName: string;
 }
 
 class DatabaseOperation {
+  // Get Database connection
   async getDatabase() {
     const db = await open({
       filename: "../database.db",
@@ -17,55 +30,64 @@ class DatabaseOperation {
     return db;
   }
 
-  async getFromPlatformChannel(channelName: string, channelID: string) {
+  // Get channelInfo from database using channelId
+  async getChannelInfo(channelId: string): Promise<ChannelInfo | string> {
     const db = await this.getDatabase();
-
-    const rows = await db.all(
-      `SELECT id FROM ${channelName} WHERE channel_id='${channelID}'`
+    const channel: ChannelInfo | undefined = await db.get(
+      `SELECT * FROM channelIds WHERE channelId = '${channelId}'`
     );
 
-    if (rows.length > 0) {
-      return true;
-    } else {
-      return false;
+    if (!channel) {
+      return "Channel Not Found";
     }
+
+    return channel;
   }
 
-  async setChannelPlatformType(channelType: ChannelType, channelID: string) {
+  // Insert channelInfo into database
+  async addChannelInfo(
+    channelId: string,
+    channelType: string,
+    channelName: string
+  ): Promise<string> {
     const db = await this.getDatabase();
+    // let resp = await db.run(
+    //   `INSERT INTO channelIds(channelId, channelType, channelName) VALUES('${channelId}', '${channelType}', '${channelName}')`
+    // );
+    let resp: ISqlite.RunResult<sqlite3.Statement>;
+    try {
+      resp = await db.run(
+        `INSERT INTO channelIds(channelId, channelType, channelName) VALUES('${channelId}', '${channelType}', '${channelName}')`
+      );
+    } catch (error: any) {
+      // If no changes made then return Error Massage
 
-    switch (channelType) {
-      case ChannelType.PC:
-        await db.run(`INSERT INTO PC_CHANNEL_IDs VALUES('${channelID}')`);
-        break;
-      case ChannelType.XBOX:
-        await db.run(`INSERT INTO XBOX_CHANNEL_IDs VALUES('${channelID}')`);
-        break;
-      case ChannelType.PS:
-        await db.run(`INSERT INTO PS_CHANNEL_IDs VALUES('${channelID}')`);
-        break;
-      default:
-        break;
+      if (error.errno === 19) {
+        return "Channel Already Exists";
+      } else {
+        return "Error Occured: " + error.code;
+      }
     }
+    return "Channel Added Successfully";
   }
 
-  async getChannelType(channelID: string): Promise<ChannelType> {
-    let channelType: ChannelType = ChannelType.OTHER;
+  // Update channelInfo to database
+  async updateChannelInfo(
+    channelId: string,
+    channelType: string,
+    channelName: string
+  ): Promise<string> {
+    let db = await this.getDatabase();
+    let resp = await db.run(
+      `UPDATE channelIds SET channelType = '${channelType}', channelName = '${channelName}' WHERE channelId = '${channelId}'`
+    );
 
-    if (await this.getFromPlatformChannel("PC_CHANNEL_IDs", channelID)) {
-      return ChannelType.PC;
+    if (!resp.changes) {
+      return "Update Failed Cannot Find Channel";
+    } else {
+      return "Channel Updated Successfully";
     }
-
-    if (await this.getFromPlatformChannel("XBOX_CHANNEL_IDs", channelID)) {
-      return ChannelType.XBOX;
-    }
-
-    if (await this.getFromPlatformChannel("PS_CHANNEL_IDs", channelID)) {
-      return ChannelType.PS;
-    }
-
-    return channelType;
   }
 }
 
-export { DatabaseOperation, ChannelType };
+export { DatabaseOperation };
