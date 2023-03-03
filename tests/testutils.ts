f5ee4a8493bb2e5bbacae6f6f94397bcda8932fc
Abu <abughalib64@gmail.com>
2023-03-03 23:54:15 +0530

    test utils

--- tests/testutils.ts
+++ tests/testutils.ts
@@ -2,10 +2,37 @@ import { CommandInteraction } from "discord.js";
 import MockDiscord from "./mockDiscord";
 
 export const defaultConfig = {
-  id: '11',
-  lang: 'en',
-  prefix: '.',
-  almanaxChannel: 'almanax',
-  partyChannel: 'listagem-de-groupos',
-  buildPreview: 'enabled'
+  id: "1",
+  lang: "en",
+  prefix: ".",
+  channel: "channel",
+  partyChannel: "party-channel",
+  buildPreview: "enabled",
+};
+
+export const optionType: any = {
+  // 0: null,
+  // 1: subCommand,
+  // 2: subCommandGroup,
+  3: String,
+  4: Number,
+  5: Boolean,
+  // 6: user,
+  // 7: channel,
+  // 8: role,
+  // 9: mentionable,
+  10: Number,
+};
+
+function getNestedOptions(options: any) {
+  return options.reduce((allOptions: any, option: any) => {
+    if (!option.options) return [...allOptions, option];
+    const nestedOptions = getNestedOptions(option.options);
+    return [option, ...allOptions, ...nestedOptions];
+  }, []);
+}
+
+function castToType(value: string, typeId: number) {
+  const type = optionType[typeId];
+  return type ? type(value) : value;
 }
