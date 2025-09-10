0f706f478a6cf6ca086227dc1900af7b8c191a4e
Abu Ghalib <abughalib64@gmail.com>
2025-09-10 18:22:57 +0530

    cleanup

--- index.ts
+++ index.ts
@@ -2,15 +2,9 @@ import {
   Client,
   GatewayIntentBits,
   IntentsBitField,
-  REST,
-  Routes,
 } from "discord.js";
 import setCommands from "./handlers/commands";
 import handleInteractions from "./handlers/interactions";
-
-const rest = new REST({ version: "10" }).setToken(
-  process.env.LOOKING_FOR_TEAM_BOT_TOKEN!
-);
 
 const client = new Client({
   intents: [
